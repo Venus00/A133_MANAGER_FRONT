3d8238111e496d48baf6360e36a30bf029d6daab
Venus00 <oussama@nextronic.io>
2023-10-23 16:34:21 +0100

    fix

--- src/features/axios/index.tsx
+++ src/features/axios/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const ApiClient = axios.create({
-  baseURL: `http://${window.location.hostname}:3000/api/v1`,
+  baseURL: `http://${window.location.hostname}:80/api/v1`,
   headers: {
     "Content-type": "application/json"
   }
