b46814935010c21d3eab570fdfe655377fa553dc
Venus00 <oussama@nextronic.io>
2023-10-24 18:19:44 +0100

    fix

--- src/components/monitor/Card.tsx
+++ src/components/monitor/Card.tsx
@@ -2,7 +2,7 @@ const Card = (props:any) => {
     return (
       <div style={{backgroundColor:props.color}} className="relative h-40 w-64 rounded-md border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
         <div className="flex justify-between">
-        <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-transparent ">
+        <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-transparent animate-bounce">
           <img src={props.icon}/>
         </div>
         <div className="text-3xl text-white">
